440c536dc92a8d9dcedf6331c960fba21f67e1d7
xxx0091 <xxx0091@163.com>
2018-10-13 20:58:09 +0800

    add project info to github

---
+++ src/main/java/com/standard/gbt/App.java
@@ -0,0 +1,13 @@
+package com.standard.gbt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
